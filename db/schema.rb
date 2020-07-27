9c292a06f0bd38b6fb7ae34cb0d8e7d28faf2227
Hano <hinsul3113@gmail.com>
2020-07-27 14:18:16 -0700

    [Lab] Test drive #new & #create actions with users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_204206) do
+ActiveRecord::Schema.define(version: 2020_07_27_205039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_04_26_204206) do
     t.integer "view_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_news_articles_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -54,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_04_26_204206) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "news_articles", "users"
   add_foreign_key "products", "users"
   add_foreign_key "reviews", "products"
   add_foreign_key "reviews", "users"
--- spec/factories/news_articles.rb
+++ spec/factories/news_articles.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
   factory :news_article do
     sequence(:title) { |n| Faker::Movies::StarWars.character + " #{n}"}
     description { Faker::Hacker.say_something_smart }
+    user
   end
 end
