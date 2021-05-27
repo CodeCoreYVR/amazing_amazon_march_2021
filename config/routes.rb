dfd5b0068a3a7a9e2862cfd4ce8f5c9f59536741
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-05-26 17:56:51 -0700

    Fixed Bugs and updated up to API Auth

--- config/routes.rb
+++ config/routes.rb
@@ -76,7 +76,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :products, only:[:index,:show,:create, :destroy]
       resource :session, only:[:create, :destroy]
-      get 'users/current'
+      resources :users, only: [:create] do
+        # get :current -> /api/v1/users/:user_id/current
+        get :current, on: :collection # -> /api/v1/users/current
+      end
     end
   end
 end
