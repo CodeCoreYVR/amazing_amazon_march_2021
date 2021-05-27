d70f90402ad82b07888146f5ac2774e55cd5028f
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-05-26 18:05:29 -0700

    API record not found

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,6 +1,8 @@
 class Api::ApplicationController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   # To send a json error message when a user types in, for example: localhost:3000/api/v1/wrongthing
   def not_found
     render(
@@ -19,5 +21,22 @@ class Api::ApplicationController < ApplicationController
     unless current_user.present?
       render(json: { status: 401 }, status: 401)
     end
+  end
+
+  protected
+  #  protected is like a private except that it prevents
+  # descendent classes from using protected methods
+
+  def record_not_found(error)
+    render(
+      status: 404,
+      json: {
+        status: 404,
+        errors: [{
+          type: error.class.to_s,
+          message: error.message
+          }]
+        }
+      )
   end
 end
